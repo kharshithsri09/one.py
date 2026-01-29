e9ff66ac877b98a41dda7c4bf2d5d73e806ab7ee
kharshithsri09 <komarthiharshith@gmail.com>
2026-01-29 09:49:00 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,67 @@
+#task
+
+#social media login
+
+'''username=input("Enter username:")
+password=int(input("Enter password:"))
+if username=="harshithsri":
+    if password==1234:
+        print("login successful")
+    else:
+        print("invalid credentials")
+else:
+    print("invalid credentials")'''
+
+#using AND 
+
+'''username=input("Enter username:")
+password=int(input("Enter password:"))
+if username=="harshithsri" and password==1234:
+    print("login successful")
+else:
+    print("invalid credentials")'''
+
+
+#swapping of 2 numbers
+'''a=20
+b=10
+a,b=b,a
+print("a value is",a)
+print("b value is",b)'''
+
+'''a=20
+b=10
+temp=a
+a=b
+b=temp
+print("a value is",a)
+print("b value is",b)'''
+
+'''a=20
+b=10
+a=a+b
+b=a-b
+a=a-b
+print("a value is",a)
+print("b value is",b)'''
+
+'''a=20
+b=10
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
+
+'''a=20
+b=10
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%f,b=%f" %(a,b))'''
+
+'''a=3.5
+b=7.9
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%.1f,b=%.1f" %(a,b))'''
